51870f5e38df94ac2e630cf6d9ee25b4b9dd8291
Ben Cheney <ben.cheney@gmail.com>
2016-12-05 16:16:25 +1030

    Breakout init code from __main__ into a class

--- chiasm_shell/chiasm_shell.py
+++ chiasm_shell/chiasm_shell.py
@@ -11,14 +11,18 @@ import config as c
 
 l = logging.getLogger('chiasm_shell.chiasm_shell')
 
+class ChiasmShell(object):
+    def run(self):
+        backend = c.get_backends()[c.get_default_backend()]
+        l.info("Chiasm Shell - {}".format(c.__VERSION__))
+        while True:
+    	    l.debug("outer loop spinning up a new shell")
+    	    l.info("Current arch is {}".format(backend.get_arch()))
+            backend.cmdloop()
+    	    if backend.launch_module is not None:
+    	        backend = backend.launch_module
+    	    else:
+    	        break
+    
 if __name__ == '__main__':
-    backend = c.get_backends()[c.get_default_backend()]
-    l.info("Chiasm Shell - {}".format(c.__VERSION__))
-    while True:
-	l.debug("outer loop spinning up a new shell")
-	l.info("Current arch is {}".format(backend.get_arch()))
-    	backend.cmdloop()
-	if backend.launch_module is not None:
-	    backend = backend.launch_module
-	else:
-	    break
+    ChiasmShell().run()
